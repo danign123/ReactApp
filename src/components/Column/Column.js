dec9456da63d4af949fecd19c1a01c0ac4901461
Daniel Ignaciuk <daniel.ignaciuk@gmail.com>
2022-02-10 14:09:24 +0100

    Problem with adding card vol.2

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,7 +1,6 @@
 import styles from './Column.module.scss';
 import Card from '../Card/Card.js';
 import CardForm from '../CardForm/CardForm.js';
-import List from '../List/List.js';
 
 const Column = props => {
     return (
@@ -10,7 +9,7 @@ const Column = props => {
             <ul className={styles.cards}>
             {props.cards.map(card => <Card key={card.id} title={card.title} />)}
             </ul>
-            <CardForm columnId={props.id} action={props.action} /> 
+            <CardForm columnId={props.id} action={props.addCard} /> 
         </article>
     );
   };
