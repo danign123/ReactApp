3df3804d05e77fc043afa22dd627d884609c5175
Daniel Ignaciuk <daniel.ignaciuk@gmail.com>
2022-02-28 14:42:18 +0100

    Create store.js and use useSelector

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,56 +1,13 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column.js';
 import ColumnForm from '../ColumnForm/ColumnForm.js';
-import { useState } from 'react';
-import shortid from 'shortid';
-
+import { useSelector } from 'react-redux';
 
 const List = () => {
-  const [columns, setColumns] = useState([
-    {
-      id: 1,
-      title: 'Books',
-      icon: 'book',
-      cards: [
-        { id: 1, title: 'This is Going to Hurt' },
-        { id: 2, title: 'Interpreter of Maladies' }
-      ]
-    },
-    {
-      id: 2,
-      title: 'Movies',
-      icon: 'film',
-      cards: [
-        { id: 1, title: 'Harry Potter' },
-        { id: 2, title: 'Star Wars' }
-      ]
-    },
-    {
-      id: 3,
-      title: 'Games',
-      icon: 'gamepad',
-      cards: [
-        { id: 1, title: 'The Witcher' },
-        { id: 2, title: 'Skyrim' }
-      ]
-    }
-  ]);
 
-  const addColumn = newColumn => {
-		setColumns([...columns, { id: shortid(), title: newColumn.title, icon: newColumn.icon, cards: [] }]);
-  };
+  const columns = useSelector(state => state.columns);
 
-  const addCard = (newCard, columnId) => {
-    const columnsUpdated = columns.map(column => {
-      if(column.id === columnId)
-        return { ...column, cards: [...column.cards, { id: shortid(), title: newCard.title }]}
-      else
-        return column
-    })
-  
-    setColumns(columnsUpdated);
-  
-  };
+  console.log('columns', columns);
 
   return (
     <div className={styles.list}>
@@ -59,9 +16,13 @@ const List = () => {
       </header>
       <p className={styles.description}>Interesting things I want to check out</p>
       <section className={styles.columns}>
-          {columns.map(column => <Column key={column.id} id={column.id}  title={column.title}  icon={column.icon} cards={column.cards} addCard={addCard} />)}
+          {columns.map(column => 
+            <Column
+              key={column.id}
+              {...column} />
+          )}
       </section>
-      <ColumnForm action={addColumn} />
+      <ColumnForm />
     </div>
   );
 };
