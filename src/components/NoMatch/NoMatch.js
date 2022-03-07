5eafaf342e116373c2371a084ec8fe38a0e53978
Daniel Ignaciuk <daniel.ignaciuk@gmail.com>
2022-03-07 14:52:45 +0100

    Add Routes and Route

---
+++ src/components/NoMatch/NoMatch.js
@@ -0,0 +1,11 @@
+import PageTitle from "../PageTitle/PageTitle";
+
+const NoMatch = () => {
+    return (
+      <div>
+        <PageTitle>404 NOT FOUND</PageTitle>
+      </div>
+    );
+};
+
+export default NoMatch;
